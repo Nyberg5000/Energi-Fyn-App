354bdd8839f0d82bafa766001897c8de17616314
Tammi Nyberg <tamminyberg@gmail.com>
2023-03-17 12:09:24 +0100

    added code to AssignmentApp

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { Alert, Button, StyleSheet, Text, View } from 'react-native';
 
 import TodayDate from './components/TodayDate';
+import AssignmentItem from './components/AssignmentItem';
 
 export default function App() {
     
@@ -12,18 +13,16 @@ export default function App() {
   return (
     <View style={styles.container} >
       <View style={styles.header} >
-        <Text>Dagens opgaver</Text>
+        <Text>
+          Dagens opgaver
+        </Text>
         <Text>
           <TodayDate/>
        </Text>
-        
-
       </View>
-      <View style= {styles.listObject}>
-        <Button color='#01553D'  onPress={()=> Alert.alert ('Åbner nyt vindue')} title='Tjek af elstander 1234'></Button>
-        <Button color='#01553D' title='Reparation på elstander 3456'></Button>
-      </View>  
-        
+     <Text>
+      <AssignmentItem/>
+     </Text>
     </View>
 
   
@@ -47,14 +46,4 @@ const styles = StyleSheet.create({
 //skal være i midten
     //flex:1,
   },
-    listObject:
-  {
-    flex:4,
-    marginTop: 20,
-    marginLeft:10,
-    marginRight:10,
-    
-  }
-  
-
 });
--- components/AssignmentItem.js
+++ components/AssignmentItem.js
@@ -1,2 +1,22 @@
 // her hentes opgaver ind fra database evt.. Disse vises i en liste - skal importeres til app.js
-//i app.js bruges pressable til at trykke på enkelte opgaver som vises i en liste 
+//i app.js bruges pressable til at trykke på enkelte opgaver som vises i en liste - en flatlist
+import { StyleSheet, View, Button } from "react-native";
+function AssignmentItem(){
+return(
+    <View style= {styles.listObject}>
+    <Button color='#01553D'  onPress={()=> Alert.alert ('Åbner nyt vindue')} title='Tjek af elstander 1234'></Button>
+    <Button color='#01553D' title='Reparation på elstander 3456'></Button>
+  </View> 
+);
+}
+export default AssignmentItem;
+
+const styles= StyleSheet.create({
+    listObject:
+  {
+    //flex:4,
+    marginTop: 20,
+    marginLeft:10,
+    marginRight:10,
+  }
+});
